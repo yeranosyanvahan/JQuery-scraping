fd26aff4888de25abbdcbc272e8dabae858f77ed
Vahan <yeranosyanvahan@gmail.com>
2021-06-30 14:32:38 +0400

    adding geoscraping

--- Browser.py
+++ Browser.py
@@ -1,21 +1,22 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
+from webdriver_manager.firefox import GeckoDriverManager
 class BROWSER:
     def jquery(self):
         with open('jquery.js','r') as file:
-            self.browser.execute_script(file.read())
+            BROWSER.browser.execute_script(file.read())
         with open('functions.js','r') as file:
-            self.browser.execute_script(file.read())
+            BROWSER.browser.execute_script(file.read())
     def url(self, url):
-        self.browser.get(url)
-    def __init__(self, url="https://google.com"):
-        browser = webdriver.Chrome(ChromeDriverManager().install())
-        browser.get(url)
-        self.browser=browser
+        BROWSER.browser.get(url)
+    def __init__(self, url="https://google.com",headless=False):
+        try:
+            BROWSER.browser.get(url)
+        except:
+            options=webdriver.FirefoxOptions()
+            options.headless=headless
+            browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=options)
+            browser.get(url)
+            BROWSER.browser=browser
         self.jquery()
     def __call__(self,string):
-        return self.browser.execute_script(string)
-    def __del__(self):
-        print("Quiting the Browser")
-        self.browser.quit()
+        return BROWSER.browser.execute_script(string)
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 This is a python package for running jquery scripts for the purpose of web scraping, take a look at the jupyter-notebook of how to scrape the amazon web page.
 <br/>
 To get started you need to install the required packages first:  <br/>
-```pip install requirements.txt``` <br/>
+```pip install -r requirements.txt``` <br/>
 You are set up, try to run basic code to see if it is working. <br/> 
 ```from Browser import BROWSER
 Browser=BROWSER()
