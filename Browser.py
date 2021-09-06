249b1c14b6ef10ed18d24f3611bda94d96a762f3
yeranosyanvahan <yeranosyanvahan@gmail.com>
2021-09-06 09:40:15 +0400

    Update browser

--- Browser.py
+++ Browser.py
@@ -1,5 +1,7 @@
-from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+
 class BROWSER:
     def jquery(self):
         with open('jquery.js','r') as file:
@@ -14,7 +16,11 @@ class BROWSER:
         except:
             options=webdriver.FirefoxOptions()
             options.headless=headless
-            browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=options)
+            try:
+             browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=options)
+            except:
+             browser= webdriver.Chrome(ChromeDriverManager().install())
+            
             browser.get(url)
             BROWSER.browser=browser
         self.jquery()
