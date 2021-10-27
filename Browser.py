31dfcfaf17b4871874236ab7bbd2925915545ce8
yeranosyanvahan <69711561+yeranosyanvahan@users.noreply.github.com>
2021-10-27 08:42:14 +0400

    Update

--- Browser.py
+++ Browser.py
@@ -1,7 +1,7 @@
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 class BROWSER:
     def jquery(self):
         with open('jquery.js','r') as file:
@@ -10,16 +10,29 @@ class BROWSER:
             BROWSER.browser.execute_script(file.read())
     def url(self, url):
         BROWSER.browser.get(url)
-    def __init__(self, url="https://google.com",headless=False):
+    def __init__(self, url="https://google.com",headless=False,proxy=""):
         try:
             BROWSER.browser.get(url)
         except:
+            Ops={}
+            if(proxy!=""):
+                prox = Proxy()
+                prox.proxy_type = ProxyType.MANUAL
+                prox.http_proxy = proxy
+
+                capabilities = webdriver.DesiredCapabilities.CHROME
+                prox.add_to_capabilities(capabilities)
+                Ops['desired_capabilities']=capabilities
+                
             options=webdriver.FirefoxOptions()
             options.headless=headless
             try:
-             browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=options)
+             Ops['executable_path']=GeckoDriverManager().install()
+             Ops['options']=options
+             browser = webdriver.Firefox(**Ops)
             except:
-             browser= webdriver.Chrome(ChromeDriverManager().install())
+             Ops['executable_path']=ChromeDriverManager().install()
+             browser= webdriver.Chrome(**Ops)
             
             browser.get(url)
             BROWSER.browser=browser
